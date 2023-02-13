2eeddf1a43d571592e08e2dfe1f91a64e46884a5
Nam39 <duongthanhnam12tin@gmail.com>
2023-02-14 01:42:54 +0700

    add Bot

---
+++ bot/server1.js
@@ -0,0 +1,41 @@
+var express = require('express');
+const Binance = require('node-binance-api');
+var fs = require('fs');
+var getData = require("./bot/index.js")
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+var server = require('http').Server(app)
+var io = require('socket.io')(server);
+app.io = io;
+server.listen(3000);
+
+io.on('connection', function(socket){
+    console.log('New connection: '+socket.id);
+})
+
+loadConfigFile('./config.json');
+
+function loadConfigFile(file){
+    var obj;
+    fs.readFile(file, 'utf8', function(err, data){
+        if(err) throw err;
+        obj = JSON.parse(data);
+        require('./routes/client')(app, obj);
+
+        const binance = new Binance().options({
+            APIKEY: 'obj.API',
+            APISECRET: 'obj.KEY'
+          });
+
+          binance.futuresMiniTickerStream( 'BTCUSDT',async (data)=> {
+            const tradeData = await getData();
+            tradeData.btcToUSD = data.close;
+            console.log(tradeData);
+            app.io.sockets.emit('server-send-price', JSON.stringify(tradeData));
+          });
+
+    });
+}
+
